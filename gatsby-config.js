170096a46999b78864fabffc5bd38af79b1df699
maxim <wedensmortume@gmail.com>
2023-12-25 13:03:55 +0900

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify");
+
 module.exports = {
   siteMetadata: {
     title: "first",
     siteUrl: `https://www.yourdomain.tld`,
   },
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+  }),
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
